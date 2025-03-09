fe17bab74fadce2d73f13932c9219ef72c534898
Nilay Türker <nilaytr89qgmail.com>
2025-03-10 00:17:55 +0300

    done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,15 @@
 import css from './Contact.module.css'
+import { FaUser } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa6";
 
 const Contact = ({ contact, onDelete }) => {
     return (
         <div>
             <li className={css.contactList}>
                 <span>{contact.name}</span>
+                <FaUser className="userIcon" size={22} />
                 <span>{contact.number}</span>
+                <FaPhone className="phoneIcon" size={22} />
                 <button className={css.contactBtn} onClick={() => onDelete(contact.id)}>Delete</button>
             </li>
         </div>
